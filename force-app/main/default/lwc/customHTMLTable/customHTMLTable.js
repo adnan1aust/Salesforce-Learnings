08339872a54d6ffcb001522727b7a4cb5482f8ad
adnan1aust <adnan1aust@users.noreply.github.com>
2021-06-30 16:41:12 +0600

    Second Commit: Data binding and sorting(UI) implemented

--- force-app/main/default/lwc/customHTMLTable/customHTMLTable.js
+++ force-app/main/default/lwc/customHTMLTable/customHTMLTable.js
@@ -1,10 +1,39 @@
-import {LightningElement,wire} from 'lwc';
+import {LightningElement,wire, track} from 'lwc';
+import accs from '@salesforce/apex/GenericController.returnAccs';
 
 export default class CustomHTMLTable extends LightningElement {
+    @track sortObjcet = {
+        Name: true,
+        AccountName: false,
+        CloseDate: false,
+        Stage: false,
+        Confidence: false,
+        Amount: false,
+        Contact : false,
+    }
     asc = true;
+    currentSortAttribute = 'Name';
+    newSortAttribute;
+    accounts = [];
+    @wire(accs)
+    acc({error, data}) {
+        if (data) {
+            this.accounts = data;
+        } else if (error) {
+            this.error = error;
+            console.log('error ===> ' + JSON.stringify(error));
+        }
+    }
     handleClick(event) {
-        event.preventDefault();
-        this.asc = !this.asc;
-        console.log(event.target.name);
+        this.newSortAttribute = event.currentTarget.dataset.id;
+        if (this.newSortAttribute != this.currentSortAttribute) {
+            this.sortObjcet[this.currentSortAttribute] = false;
+            this.sortObjcet[this.newSortAttribute] = true;
+            this.asc = true;
+            this.currentSortAttribute = this.newSortAttribute;
+        } else {
+            this.asc = !this.asc;
+        }
+        console.log(event.currentTarget.dataset.id);
     }
 }
